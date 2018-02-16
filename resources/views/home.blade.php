b26061450563cfbf149d15d00c20c48d2bdd0a87
tarikuli <tarikuli@yahoo.com>
2018-02-15 20:23:07 -0500

    Welcome to Surver App

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,10 @@
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
-				<div class="panel-heading">Please Answer these Q</div>
+				<div class="panel-heading">Welcome to Surver App</div>
 				<div class="panel-body">
-					Defult Home Page
+					<a class = "btn btn-success btn-sm pull-left" href = "{{url('/questions/index')}}">Go to List</a>
+					<a class = "btn btn-success btn-sm pull-right" href = "{{url('/questions/create')}}">Create Survey</a>
 				</div>
 			</div>
 		</div>
